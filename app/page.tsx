d47d632928f24e22ef4e1731e49372650349886a
shivimishra <shivimishra130103@gmail.com>
2024-03-23 00:53:45 +0530

    top stories

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,27 @@
 import DiscussionForum from "@/components/shared/discussion";
+import MarketStories from "@/components/shared/stories";
 
 export default function Home() {
   return (
     <main className="flex w-full min-h-screen p-10 md:p-20 md:max-w-screen-2xl md:mx-auto overflow-x-auto">
       <div className="grid grid-cols-1 sm:grid-cols-7 gap-3 justify-between h-full w-full">
-      <div className="w-full col-span-5 pt-10 sm:pt-0">
-        <p className="text-4xl text-bold text-primary/60 font-serif">DISCUSSION FORUM</p>
-        <DiscussionForum />
-        <DiscussionForum />
-        <DiscussionForum />
-        <DiscussionForum />
-      </div>
-      <div className="w-full col-span-4 lg:col-span-2 hidden sm:block">
-       Market Stories
-      </div> 
-
+        <div className="w-full col-span-5 pt-10 sm:pt-0">
+          <p className="text-4xl text-bold text-primary/60 font-serif">
+            DISCUSSION FORUM
+          </p>
+          <DiscussionForum />
+          <DiscussionForum />
+          <DiscussionForum />
+          <DiscussionForum />
+        </div>
+        <div className="w-full sm:col-span-3 md:col-span-2 ">
+          <p className="text-4xl text-bold text-primary/60 font-serif">
+            Market Stories
+          </p>
+          <MarketStories />
+          <MarketStories />
+          <MarketStories />
+        </div>
       </div>
     </main>
   );
---
+++ components/shared/stories.tsx
@@ -0,0 +1,34 @@
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import Image from "next/image";
+
+export default function MarketStories() {
+  return (
+    <Card className="my-4 bg-secondary">
+      <CardHeader>
+        <CardTitle className="text-lg">Top Stories</CardTitle>
+      </CardHeader>
+      <CardContent>
+      <div className="flex-shrink-0 h-32 w-full relative overflow-hidden">
+          <Image
+            src="/img.png"
+            alt="img"
+            layout="fill"
+            objectFit="cover"
+          />
+        </div>
+        <p className="text-justify font-sans pt-2">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quidem
+          officia accusantium, modi atque non quisquam numquam facere
+          consectetur deserunt, ab dolorem fuga vel impedit enim eos. Sit eos
+          amet tenetur.
+        </p>
+      </CardContent>
+    </Card>
+  );
+}
